9d1259952b5e39be1b89a8ab376bd670b25436a6
IgorNascAlves <igor.nascimento.flipe@gmail.com>
2021-10-16 19:43:54 -0300

    download temp20-21 completo e função para downloads futuros

--- scripts/leitura_jogos.py
+++ scripts/leitura_jogos.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import pickle
 import sys
 import os
 
@@ -73,10 +74,163 @@ def jogo_todos_q(GameID):
 
 	df_players_all.to_csv(f'dados/{nome_pasta_jogo}/{gameId}_players_statistics.csv', index=False)
 
-_, GameID = sys.argv
+def temporada_todos_q(temporada, season='002'):
 
-jogo_todos_q('0042000' + GameID)
+	rangeType = '1'
+	startRange = '0'
+	endRange = '31800'
+
+	headers = retorna_header() 
+	df_players_all = pd.DataFrame()
+	
+	ID = 1
+	GameID = season + temporada + f'{ID:05d}'
+	url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
+	url_stats = f'{url_base}endPeriod=1&endRange={endRange}&rangeType={rangeType}&startPeriod=1&startRange={startRange}'
+	status_code_site=requests.get(url_stats, headers=retorna_header()).status_code
+
+	while status_code_site == 200:
+		print(GameID)
+		status_code = 200
+		Q = 1		
+
+		while status_code != None:
+
+			startPeriod, endPeriod = Q, Q
+			
+			url_stats = f'{url_base}endPeriod={endPeriod}&endRange={endRange}&rangeType={rangeType}&startPeriod={startPeriod}&startRange={startRange}'
+
+			y = requests.get(url_stats, headers=headers).json()
+			
+			status_code = y['boxScoreTraditional']['homeTeam']['teamName']
+
+			if status_code != None:
+
+				name_homeTeam = y['boxScoreTraditional']['homeTeam']['teamName']
+				name_awayTeam = y['boxScoreTraditional']['awayTeam']['teamName']
+				gameId = y['boxScoreTraditional']['gameId']
+
+				df_players_homeTeam = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['players'])
+				df_players_homeTeam['teamName'] = name_homeTeam
+				df_players_awayTeam = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['players'])
+				df_players_awayTeam['teamName'] = name_awayTeam
+				df_players = df_players_awayTeam.append(df_players_homeTeam)
+				
+				df_players['gameId'] = gameId
+				df_players['Q'] = Q
+				
+				df_players_all = df_players_all.append(df_players)
+
+			Q += 1
+		ID += 1
+		GameID = season + temporada + f'{ID:05d}'
+		url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
+		url_stats = f'{url_base}endPeriod=1&endRange={endRange}&rangeType={rangeType}&startPeriod=1&startRange={startRange}'
+		status_code_site=requests.get(url_stats, headers=retorna_header()).status_code
+
+	df_players_all.to_csv(f'dados/{season}_{temporada}_all_Q.csv', index=False)
+
+def baixar_temporada_todos_q(temporada, season='002'):
+
+	rangeType = '1'
+	startRange = '0'
+	endRange = '31800'
+
+	headers = retorna_header()
+	
+	ID = 1
+	GameID = season + temporada + f'{ID:05d}'
+	url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
+	url_stats = f'{url_base}endPeriod=1&endRange={endRange}&rangeType={rangeType}&startPeriod=1&startRange={startRange}'
+	status_code_site=requests.get(url_stats, headers=retorna_header()).status_code
+
+	while status_code_site == 200:
+		print(GameID)
+		status_code = 200
+		Q = 1		
+
+		while status_code != None:
+
+			startPeriod, endPeriod = Q, Q
+			
+			url_stats = f'{url_base}endPeriod={endPeriod}&endRange={endRange}&rangeType={rangeType}&startPeriod={startPeriod}&startRange={startRange}'
+
+			y = requests.get(url_stats, headers=headers).json()
+			
+			status_code = y['boxScoreTraditional']['homeTeam']['teamName']
+
+			if status_code != None:
+				gameId = y['boxScoreTraditional']['gameId']
+				a_file = open(f"dados/dict/{gameId}_{Q}.pkl", "wb")
+				pickle.dump(y, a_file)
+				a_file.close()
+
+			Q += 1
+		ID += 1
+		GameID = season + temporada + f'{ID:05d}'
+		url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
+		url_stats = f'{url_base}endPeriod=1&endRange={endRange}&rangeType={rangeType}&startPeriod=1&startRange={startRange}'
+		status_code_site=requests.get(url_stats, headers=retorna_header()).status_code
+
+def montar_temporada_todos_q(temporada, num_jogos, season='002'):
+	
+	df_players_all = pd.DataFrame()	
+	
+	for ID in range(1, num_jogos+1):
+		GameID = season + temporada + f'{ID:05d}'	
+		print(GameID)
+
+		Q = 1		
+		y = True		
+
+		while y != None:
+			
+			try:
+				a_file = open(f"dados/dict/{GameID}_{Q}.pkl", "rb")
+			except:
+				y = None
+			else:				
+				y = pickle.load(a_file)
+
+			if y != None:				
+
+				name_homeTeam = y['boxScoreTraditional']['homeTeam']['teamName']
+				name_awayTeam = y['boxScoreTraditional']['awayTeam']['teamName']
+				gameId = y['boxScoreTraditional']['gameId']
+
+				df_players_homeTeam = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['players'])
+				df_players_homeTeam['teamName'] = name_homeTeam
+				df_players_awayTeam = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['players'])
+				df_players_awayTeam['teamName'] = name_awayTeam
+				df_players = df_players_awayTeam.append(df_players_homeTeam)
+				
+				df_players['gameId'] = gameId
+				df_players['Q'] = Q
+				
+				print(gameId, Q, sep=' ', end='\n')
+
+				df_players_all = df_players_all.append(df_players)
+			Q += 1
+		ID += 1
+	
+	df_players_all.to_csv(f'dados/{season}_{temporada}_all_Q.csv', index=False)
+	print(df_players_all.shape)
+
+# baixar_temporada_todos_q('20')
+montar_temporada_todos_q('20', 1080)
+# _, GameID = sys.argv
+###
+# df_id_date = pd.read_csv('dados/nets_id_game_season_20_21.csv')
+# for id_game, _ in df_id_date.values:
+# 	jogo_todos_q('00' + str(id_game))
+###
+# jogo_todos_q('0042000' + GameID)
 
 #0042000217 NetsXBucks
 #0042000402 SunsXBucks
-#0042000406 BucksXSuns
+#004 20 00406 BucksXSuns
+
+
+##002 = regular season
+##20 = temporada 20/21
+##00000 = num jogo
